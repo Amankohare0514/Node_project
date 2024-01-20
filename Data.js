bb40d82837347151d44475cb5868323755354c28
Aman kohare <amankohare@gmail.com>
2024-01-21 00:59:02 +0530

    adding mongodb url

---
+++ Data.js
@@ -0,0 +1,104 @@
+const UserData = [
+        {
+          "fname": "1",
+          "lname": "2",
+          "age": "3"
+        },
+        {
+          "fname": "4",
+          "lname": "5",
+          "age": "6"
+        },
+        {
+          "fname": "7",
+          "lname": "8",
+          "age": "9"
+        },
+        {
+          "fname": "10",
+          "lname": "11",
+          "age": "12"
+        },
+        {
+          "fname": "13",
+          "lname": "14",
+          "age": "15"
+        },
+        {
+          "fname": "16",
+          "lname": "17",
+          "age": "18"
+        },
+        {
+          "fname": "19",
+          "lname": "20",
+          "age": "21"
+        },
+        {
+          "fname": "22",
+          "lname": "23",
+          "age": "24"
+        },
+        {
+          "fname": "25",
+          "lname": "26",
+          "age": "27"
+        },
+        {
+          "fname": "28",
+          "lname": "29",
+          "age": "30"
+        },
+        {
+          "fname": "31",
+          "lname": "32",
+          "age": "33"
+        },
+        {
+          "fname": "34",
+          "lname": "35",
+          "age": "36"
+        },
+        {
+          "fname": "37",
+          "lname": "38",
+          "age": "39"
+        },
+        {
+          "fname": "40",
+          "lname": "41",
+          "age": "42"
+        },
+        {
+          "fname": "43",
+          "lname": "44",
+          "age": "45"
+        },
+        {
+          "fname": "46",
+          "lname": "47",
+          "age": "48"
+        },
+        {
+          "fname": "49",
+          "lname": "50",
+          "age": "51"
+        },
+        {
+          "fname": "52",
+          "lname": "53",
+          "age": "54"
+        },
+        {
+          "fname": "55",
+          "lname": "56",
+          "age": "57"
+        },
+        {
+          "fname": "58",
+          "lname": "59",
+          "age": "60"
+        }
+      ]
+
+module.exports = {UserData}
